dd99be30553086205c572c50f69d4265a8164c1d
Steve Popoola <stevepop@gmail.com>
2018-02-02 08:33:46 +0000

    Minr changed to the styling of the temporary landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,12 +17,16 @@
 
 <div class="bgimg">
     <div class="middle">
-        <h1>Comming Soon!</h1>
+        <h1>Laravelphp.uk Coming Soon!</h1>
         <hr>
-        <p>Join us as we build build this website at #Larahack.</p>
-        <p>February 3 - 4 2018!</p>
+
+        <div class="info">
+            <p>Join us as we build this website in a weekend  #Larahack.</p>
+            <p>February 3 - 4 2018!</p>
+        </div>
+        
     </div>
-    <div class="bottom-center text-center">
+    <div class="bottom-center">
         <p>Laravel UK - The most exciting coding group in the UK</p>
     </div>
 </div>
