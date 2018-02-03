e4104d47393f8363a39fd1e3df80d662f53a2d45
Steve Popoola <stevepop@gmail.com>
2018-02-03 12:07:08 +0000

    Implemented user registration

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class)->create([
+            'name' => 'John',
+            'email' => 'john@doe.com',
+            'password' => bcrypt('secret')
+        ]);
+    }
+}
