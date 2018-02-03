69651be8a19560037fc669b616a812eabcafdfbe
Steve Popoola <stevepop@gmail.com>
2018-02-03 12:42:33 +0000

    Removed Passport. Decision made to use web middleware for backend ajax requests

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,10 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
