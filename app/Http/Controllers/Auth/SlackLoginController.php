658efbce687c163927d251a416b93692f1ef3736
Joe Priest <joe.priest@live.co.uk>
2018-02-03 15:46:21 +0000

    use name and email to check for existing users, and set password only if creating, during user sign in

--- app/Http/Controllers/Auth/SlackLoginController.php
+++ app/Http/Controllers/Auth/SlackLoginController.php
@@ -34,6 +34,7 @@ class SlackLoginController extends Controller
         $user = User::firstOrCreate([
             'name' => $slackUser->getName(),
             'email' => $slackUser->getEmail(),
+        ],[
             'password' => bcrypt(str_random(16)),
         ]);
 
