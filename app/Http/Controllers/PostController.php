145cdf0c1e5ac6380384fdcfb3599ce4744a327b
Steve Popoola <stevepop@gmail.com>
2018-02-03 20:01:44 +0000

    Removed methods not needed in PostController Removed blog routes file as it is not needed since we don't have many routes Blog view will be migrated into Vue components and blade files will be deleted Corrected wrong imports of Post model in Controller Removed all Post related tests as they will not work for Ajax requests

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -30,16 +30,6 @@ class PostController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return \view('posts.create');
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -58,18 +48,6 @@ class PostController extends Controller
         return response()->json([
             'post' => $post
         ]);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Post $post)
-    {
-        return view('posts.show', compact('post'));
     }
 
     /**
