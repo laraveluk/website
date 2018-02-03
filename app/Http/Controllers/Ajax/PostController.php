3df3eec453d2a0b63e0d79c54551d6d0883ed64a
Joe Priest <joe.priest@live.co.uk>
2018-02-03 18:47:54 +0000

    use correct name for Post Controller

--- app/Http/Controllers/Ajax/PostController.php
+++ app/Http/Controllers/Ajax/PostController.php
@@ -7,7 +7,7 @@ use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class MemberController extends Controller
+class PostController extends Controller
 {
     public function index()
     {
