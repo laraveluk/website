19472dd5db71f5f7a8d2c5881fc4f84fdc78e476
Michael Burton <me@mikeylicio.us>
2018-02-03 14:55:02 +0000

    Add unit test for the Post Model. Set up Author Relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    /**
+     * A post belongs to a user
+     * 
+     * @return Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function author()
+    {
+        return $this->belongsTo(\App\Models\User::class, 'user_id');
+    }
 }
--- tests/Unit/PostTest.php
+++ tests/Unit/PostTest.php
@@ -3,18 +3,55 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class PostTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use RefreshDatabase;
+
+    protected $post;
+
+    public function setUp()
     {
-        $this->assertTrue(true);
+        parent::setUp();
+
+        $this->post = factory(Post::class)->make();
+    }
+
+    /** @test */
+    public function it_has_a_title()
+    {
+        $this->assertInternalType("string", $this->post->title);
+        $this->assertNotNull($this->post->title);
+    }
+
+    /** @test */
+    public function it_has_a_slug()
+    {
+        $this->assertInternalType("string", $this->post->slug);
+        $this->assertNotNull($this->post->slug);
+    }
+
+    /** @test */
+    public function it_has_a_body()
+    {
+        $this->assertInternalType("string", $this->post->body);
+        $this->assertNotNull($this->post->body);
+    }
+
+    /** @test */
+    public function it_has_a_user_id()
+    {
+        $this->assertInternalType("int", $this->post->user_id);
+        $this->assertNotNull($this->post->user_id);
+    }
+
+    /** @test */
+    public function it_has_an_author()
+    {
+        $this->assertInstanceOf(User::class, $this->post->author);
     }
 }
