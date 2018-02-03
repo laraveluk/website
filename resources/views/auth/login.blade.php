a9fd508e60dc849e4c47a059e6965d4bce450145
SymfonyCoda <barry.j.holland@hotmail.com>
2018-02-03 12:17:16 +0000

    add initial design for login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -67,3 +67,44 @@
     </div>
 </div>
 @endsection
+<div class="container">
+    <div class="flex content-center">
+        <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                                {{ csrf_field() }}
+            <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+                <div class="mb-4">
+                    <label class="block text-grey-darker text-sm font-bold mb-2" for="username">
+                        Username
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="username" type="text" name="username" value="{{ old('username') }}" placeholder="Username">
+        
+                    @if ($errors->has('username'))
+                    <span class="">
+                        <strong>{{ $errors->first('username') }}</strong>
+                    </span>
+                    @endif
+                </div>
+
+
+
+
+
+                <div class="mb-6">
+                    <label class="block text-grey-darker text-sm font-bold mb-2" for="password">
+                        Password
+                    </label>
+                    <input class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password" placeholder="******************">
+                    <p class="text-red text-xs italic">Please choose a password.</p>
+                </div>
+                <div class="flex items-center justify-between">
+                    <button class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded" type="button">
+                        Sign In
+                    </button>
+                    <a class="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker" href="{{ route('password.request') }}">
+                        Forgot Password?
+                    </a>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
