fa546e256f52e16479482f9b9efb4b9f7bf057e6
SymfonyCoda <barry.j.holland@hotmail.com>
2018-02-03 16:28:56 +0000

    add colors and styles to login and register boxes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,110 +1,48 @@
 @extends('frontend.master')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
-<div class="container">
+<div class="container content-center">
     <div class="flex content-center">
         <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                                 {{ csrf_field() }}
-            <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+            <div class="bg-blue-dark shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col text-blue-lightest">
+ 
+
                 <div class="mb-4">
-                    <label class="block text-grey-darker text-sm font-bold mb-2" for="username">
-                        Username
+                    <label class="block text-blue-lightest text-sm font-bold mb-2" for="email">
+                        Email
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="username" type="text" name="username" value="{{ old('username') }}" placeholder="Username">
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-blue-lightest" id="name" type="text" name="email" value="{{ old('email') }}" placeholder="Email">
         
-                    @if ($errors->has('username'))
+                    @if ($errors->has('email'))
                     <span class="">
-                        <strong>{{ $errors->first('username') }}</strong>
+                        <strong>{{ $errors->first('email') }}</strong>
                     </span>
                     @endif
                 </div>
 
 
 
-
-
                 <div class="mb-6">
-                    <label class="block text-grey-darker text-sm font-bold mb-2" for="password">
+                    <label class="block text-blue-lightest text-sm font-bold mb-2" for="password">
                         Password
                     </label>
                     <input class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password" placeholder="******************">
-                    <p class="text-red text-xs italic">Please choose a password.</p>
+                    <!-- <p class="text-red text-xs italic">Please choose a password.</p> -->
                 </div>
                 <div class="flex items-center justify-between">
-                    <button class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded" type="button">
+                    <button class="bg-red hover:bg-red-dark text-white font-bold py-2 px-4 rounded" type="button">
                         Sign In
                     </button>
-                    <a class="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker" href="{{ route('password.request') }}">
-                        Forgot Password?
+                    <a class="inline-block align-baseline font-bold text-sm text-white hover:text-blue-darker" href="{{ route('password.request') }}">
+                        Forgot </br> Password?
                     </a>
                 </div>
             </div>
         </form>
     </div>
 </div>
+
+@endsection
