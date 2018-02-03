7ce832f9cba5de9129da07929774d32c4a66de05
Steve Popoola <stevepop@gmail.com>
2018-02-03 10:35:14 +0000

    Added Auth Scafolding for the project

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,6 +6,16 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function homePage()
     {
         return view('frontend.index');
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,5 @@
 
 Route::get('/', 'PagesController@homePage')->name('home');
 Route::get('/laravel-people', 'PagesController@laravelPeople')->name('laravelpeeps');
+Auth::routes();
+
