cf8e9ea512c01a188b3081970970492a2d5f4fe2
Steve Popoola <stevepop@gmail.com>
2018-02-04 17:48:31 +0000

    Updated Login and register form designs

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,96 +3,102 @@
 @include('frontend.partials.header')
 
 @section('content')
+    <div class="py-6 pb-8 pt-20 md:pt-16 w-full">
+        <div class="h-100 w-full flex items-center justify-center bg-grey-lighter font-sans">
+            <div class="bg-blue-dark rounded shadow p-6 m-4 md:w-full sm:w-full lg:w-3/4 lg:max-w-lg">
+                <form role="form" class="mb-4">
+                    <div class="lg:w-1/8 md:w-1/4 mb-6">
+                        <div class="text-white p-2">Signup with </div>
+                        <a href="{{ route('login.slack') }}">
+                            <img src="{{ asset('/images/slack-button.png') }}"
+                                 class="rounded py-2 px-3 bg-white hover:bg-green-lighter w-32">
+                        </a>
+                    </div>
 
-
-<div class="flex justify-center items-center w-screen h-screen">
-    <div class="flex content-center">
-        <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                                {{ csrf_field() }}
-            <div class="text-blue-lightest shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col bg-blue-dark ">
-
-                <div class="flex items-center justify-between">
-                    <a href="{{ route('login.slack') }}"><img src="{{ asset('/images/slack-button.png') }}" class="shadow appearance-none border rounded w-full py-2 px-3 bg-white hover:bg-grey"></a>
-                </div>
-                </br>
-                <div class="text-white font-bold flex content-center">
-                OR
-                </div>
-                </br></br>
-                <div class="mb-4">
-                    <label class="block text-blue-lightest text-sm font-bold mb-2" for="name">
-                        Username
-                    </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-blue-lightest" id="name" type="text" name="name" value="{{ old('name') }}" placeholder="Username">
-        
-                    @if ($errors->has('name'))
-                    <span class="">
-                        <strong>{{ $errors->first('name') }}</strong>
+                    <div class="md:flex md:items-center mb-6">
+                        <div class="md:w-1/3">
+                            <label for="email"
+                                   class="block text-grey font-bold md:text-right mb-1 md:mb-0 pr-4">
+                                Username
+                            </label>
+                        </div>
+                        <div class="md:w-2/3">
+                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-blue-lightest" id="username" type="text" name="email" value="{{ old('username') }}" placeholder="Username">
+                            @if ($errors->has('username'))
+                                <span class="">
+                        <strong>{{ $errors->first('username') }}</strong>
                     </span>
-                    @endif
-                </div>
+                            @endif
+                        </div>
+                    </div>
 
-                <div class="mb-4">
-                    <label class="block text-blue-lightest text-sm font-bold mb-2" for="email">
-                        Email
-                    </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-blue-lightest" id="email" type="text" name="email" value="{{ old('email') }}" placeholder="Email">
-        
-                    @if ($errors->has('email'))
-                    <span class="">
+                    <div class="md:flex md:items-center mb-6">
+                        <div class="md:w-1/3">
+                            <label for="email"
+                                 class="block text-grey font-bold md:text-right mb-1 md:mb-0 pr-4">
+                                Email
+                            </label>
+                        </div>
+                        <div class="md:w-2/3">
+                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-blue-lightest" id="name" type="text" name="email" value="{{ old('email') }}" placeholder="Email">
+                            @if ($errors->has('email'))
+                                <span class="">
                         <strong>{{ $errors->first('email') }}</strong>
                     </span>
-                    @endif
-                </div>
-
-                <div class="mb-4">
-                    <label class="block text-blue-lightest text-sm font-bold mb-2" for="location">
-                        Location
-                    </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-blue-lightest" id="location" type="text" name="location" value="{{ old('location') }}" placeholder="Location">
-        
-                    @if ($errors->has('location'))
-                    <span class="">
-                        <strong>{{ $errors->first('location') }}</strong>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="md:flex md:items-center mb-6">
+                        <div class="md:w-1/3">
+                            <label for="email"
+                                   class="block text-grey font-bold md:text-right mb-1 md:mb-0 pr-4">
+                                Location
+                            </label>
+                        </div>
+                        <div class="md:w-2/3">
+                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-blue-lightest" id="location" type="text" name="location" value="{{ old('location') }}" placeholder="Where are you based?">
+                            @if ($errors->has('location'))
+                                <span class="">
+                        <strong>{{ $errors->first('username') }}</strong>
                     </span>
-                    @endif
-                </div>
-
-
-
-
-
-                <div class="mb-6">
-                    <label class="block text-blue-lightest text-sm font-bold mb-2" for="password">
-                        Password
-                    </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker mb-3" id="password" name="password" type="password" placeholder="******************">
-                    @if ($errors->has('password'))
-                    <span class="">
-                        <strong>{{ $errors->first('password') }}</strong>
-                    </span>
-                    @endif
-
-                    <label class="block text-blue-lightest text-sm font-bold mb-2" for="password-confirm">
-                        Confirm Password
-                    </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker mb-3" id="password-confirm" type="password" name="password_confirmation" placeholder="******************">
-                    @if ($errors->has('password'))
-                    <span class="">
-                        <strong>{{ $errors->first('password') }}</strong>
-                    </span>
-                    @endif
-                </div>
-                <div class="flex items-center justify-between">
-                <button class="bg-red hover:bg-red-dark text-white font-bold py-2 px-4 rounded" type="submit">
-                    Register
-                </button>
-                </div>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="md:flex md:items-center mb-6">
+                        <div class="md:w-1/3">
+                            <label for="password"
+                                   class="block text-grey font-bold md:text-right mb-1 md:mb-0 pr-4">
+                                Password
+                            </label>
+                        </div>
+                        <div class="md:w-2/3">
+                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker mb-3" id="password" name="password" type="password" placeholder="******************">
+                        </div>
+                    </div>
+                    <div class="md:flex md:items-center mb-6">
+                        <div class="md:w-1/3">
+                            <label for="password_confirmation"
+                                   class="block text-grey font-bold md:text-right mb-1 md:mb-0 pr-4">
+                                Confirm Password
+                            </label>
+                        </div>
+                        <div class="md:w-2/3">
+                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker mb-3" id="password_confirmation" name="password_confirmation" type="password" placeholder="******************">
+                        </div>
+                    </div>
+                    <div class="md:flex md:items-center">
+                        <div class="md:w-1/3"></div>
+                        <div class="md:w-2/3">
+                            <button type="submit"
+                                    class="shadow bg-red hover:bg-purple-light text-white font-bold py-2 px-4 rounded">
+                                Login
+                            </button>
+                        </div>
+                    </div>
+                </form>
             </div>
-        </form>
+        </div>
     </div>
-</div>
-
 @endsection
 
 
