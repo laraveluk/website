1385f4c295ea6514ceb14d1552a3fc434ef269fa
SymfonyCoda <barry.j.holland@hotmail.com>
2018-02-04 12:44:10 +0000

    add password reset page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
                                 {{ csrf_field() }}
             <div class="bg-blue-dark shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col text-blue-lightest">
                 <div class="flex items-center justify-between">
-                <a href="{{ route('login.slack') }}"><img src="{{ asset('/images/slack-button.png') }}" class="shadow appearance-none border rounded w-full py-2 px-3 bg-white"></a>
+                <a href="{{ route('login.slack') }}"><img src="{{ asset('/images/slack-button.png') }}" class="shadow appearance-none border rounded w-full py-2 px-3 bg-white hover:bg-grey"></a>
                 </div>
                 </br>
                 <div class="text-white font-bold flex content-center">
@@ -36,13 +36,12 @@
                         Password
                     </label>
                     <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker mb-3" id="password" name="password" type="password" placeholder="******************">
-                    <!-- <p class="text-red text-xs italic">Please choose a password.</p> -->
                 </div>
                 <div class="flex items-center justify-between">
                     <button class="bg-red hover:bg-red-dark text-white font-bold py-2 px-4 rounded" type="submit">
                         Sign In
                     </button>
-                    <a class="inline-block align-baseline font-bold text-sm text-blue-lightest hover:text-grey-dark" href="{{ route('password.request') }}">
+                    <a class="inline-block appearance-none align-baseline font-bold text-sm text-blue-lightest hover:text-grey-dark" href="{{ route('password.request') }}">
                         Forgot </br> Password?
                     </a>
                 </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
             <div class="text-blue-lightest shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col bg-blue-dark ">
 
                 <div class="flex items-center justify-between">
-                    <a href="{{ route('login.slack') }}"><img src="{{ asset('/images/slack-button.png') }}" class="shadow appearance-none border rounded w-full py-2 px-3 bg-white"></a>
+                    <a href="{{ route('login.slack') }}"><img src="{{ asset('/images/slack-button.png') }}" class="shadow appearance-none border rounded w-full py-2 px-3 bg-white hover:bg-grey"></a>
                 </div>
                 </br>
                 <div class="text-white font-bold flex content-center">
