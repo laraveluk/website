5e4cf6d94bf7900cd2698efdfc1e3c0a07a7fa58
Steve Popoola <stevepop@gmail.com>
2018-02-04 21:49:35 +0000

    Pass arguments into deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,31 @@
 #!/bin/sh
 
-whoami
-# activate maintenance mode
-php artisan down
+env = $1
 
-# update source code
-git pull
+if [ $env -eq 'github' ]
+then
+    # activate maintenance mode
+    php artisan down
 
-# update PHP dependencies
-export COMPOSER_HOME='/tmp/composer'
-composer install --no-interaction --no-dev --prefer-dist
-	# --no-interaction	Do not ask any interactive question
-	# --no-dev		Disables installation of require-dev packages.
-	# --prefer-dist		Forces installation from package dist even for dev versions.
+    # update source code
+    git pull
+
+    # update PHP dependencies
+    export COMPOSER_HOME='/tmp/composer'
+    composer install --no-interaction --no-dev --prefer-dist
+        # --no-interaction	Do not ask any interactive question
+        # --no-dev		Disables installation of require-dev packages.
+        # --prefer-dist		Forces installation from package dist even for dev versions.
+
+else
+    # Install frontend assets
+    yarn
+
+    yarn run production
+fi
 
 # update database
 php artisan migrate --force
-	# --force		Required to run when in production.
 
-# Install frontend assets
-yarn
-
-yarn run production
 # stop maintenance mode
 php artisan up
