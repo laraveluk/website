83798b9c0837a8bac00f0d7fc515cdd4aec71c0b
Michael Burton <me@mikeylicio.us>
2018-02-04 16:43:26 +0000

    Adjust postcontroller to handle tags

--- app/Http/Controllers/Ajax/PostController.php
+++ app/Http/Controllers/Ajax/PostController.php
@@ -56,14 +56,18 @@ class PostController extends Controller
     {
         $post = new Post;
 
-        $tags = isset($request->tags) ? explode(',', $request->tags) : [];
-        
+        $tags = explode(',', $request->data['tags']);
+
         $post->title = $request->data['title'];
         $post->body = $request->data['body'];
         $post->post_type = $request->data['post_type'] ?? 'post';
         $post->user_id = auth()->id();
-        $post->tags = $tags;
+        // $post->tags = $tags;
         $post->save();
+
+        foreach ($tags as $tag) {
+            $post->attachTag($tag);
+        }
 
         if (auth()->user()) {
             Log::debug(auth()->user()->name . " created post {$post->id}");
