b45e11d022c8c24b9d0dd5ce0f8bb43460ede2d9
Joe Priest <joe.priest@live.co.uk>
2018-02-04 22:28:34 +0000

    add Dropdown Vue component

---
+++ resources/assets/js/components/Dropdown.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="relative">
+        <div role="button" class="inline-block select-none" @click="open = !open">
+            <slot name="link"></slot>
+        </div>
+        <div class="absolute pin-l mt-px" v-show="open">
+            <slot name="dropdown"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                open: false
+            }
+        }
+    }
+</script>
