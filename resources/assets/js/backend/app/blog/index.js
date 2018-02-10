5db91505ea851b04125a0d3bc9284a2438c9b395
Michael Burton <me@mikeylicio.us>
2018-02-10 18:11:01 +0000

    Remove /blog from Vue Router

--- resources/assets/js/backend/app/blog/index.js
+++ resources/assets/js/backend/app/blog/index.js
@@ -1,4 +1,4 @@
 import Vue from 'vue'
 
-export const PostsList = Vue.component('PostsList', require('./PostsList.vue'))
+// export const PostsList = Vue.component('PostsList', require('./PostsList.vue'))
 export const PostCreate = Vue.component('PostCreate', require('./PostCreate.vue'))
--- resources/assets/js/backend/app/blog/routes/index.js
+++ resources/assets/js/backend/app/blog/routes/index.js
@@ -2,11 +2,6 @@ import { PostsList, PostShow, PostCreate } from '../components/index'
 
 export default [
     {
-        path: '/blog',
-        component: PostsList,
-        name: 'posts-lists',
-    },
-    {
         path: '/blog/create',
         component: PostCreate,
         name: 'post-create'
