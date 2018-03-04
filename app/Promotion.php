97881d519a66a5f6d61814b3d8a42f2404387ff3
Josh Price <josh.priceonline@gmail.com>
2018-03-04 20:03:04 +0000

    Migrations and Model for Promotion

---
+++ app/Promotion.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Promotion extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name', 'url', 'published', 'description'
+    ];
+}
---
+++ database/migrations/2018_03_04_195413_create_promotions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePromotionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('promotions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->string('name');
+            $table->string('description', 140);
+            $table->string('url');
+            $table->boolean('published')->default(true);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('promotions');
+    }
+}
