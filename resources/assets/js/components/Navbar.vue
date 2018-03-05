8c41e27cf70bba51b4122ed2b5f2286aae32a2b1
Steve Popoola <stevepop@gmail.com>
2018-03-05 07:08:00 +0000

    Some tweaks in the ab out page Added About us to the Nav links Removed Blog dropdown - no longer needed

--- resources/assets/js/components/Navbar.vue
+++ resources/assets/js/components/Navbar.vue
@@ -13,19 +13,9 @@
             <input type="checkbox" id="mainNavTogger" class="hidden">
             <nav class="invisible fixed pin flex flex-col md:visible md:static md:flex md:flex-row justify-center items-center text-center text-xl md:text-sm lg:text-base bg-blue md:bg-transparent z-10">
                 <label for="mainNavTogger" class="w-10 h-10 flex items-center justify-center p-3 mt-8 mr-6 absolute pin-t pin-r text-2xl leading-none text-white bg-red rounded-full md:hidden">&times;</label>
-                <a href="/" class="text-blue-lightest hover:text-white no-underline p-3 md:px-2">Home</a>
-                <dropdown>
-                <span slot="link" class="appearance-none flex items-center inline-block text-white font-medium bg-blue-navy hover:bg-blue-navy px-4 py-2 rounded">
-                    <span class="mr-1">Blog</span>
-                    <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                    </svg>
-                </span>
-                    <div slot="dropdown" class="bg-white shadow rounded border overflow-hidden">
-                        <a href="/blog" class="no-underline block px-4 py-3 border-b text-grey-darkest bg-white hover:text-white hover:bg-blue whitespace-no-wrap">Posts</a>
-                        <a href="/blog/interviews" class="no-underline block px-4 py-3 border-b text-grey-darkest bg-white hover:text-white hover:bg-blue whitespace-no-wrap">Interviews</a>
-                    </div>
-                </dropdown>
+               <a href="/" class="text-blue-lightest hover:text-white no-underline p-3 md:px-2">Home</a>
+               <a href="/about" class="text-blue-lightest hover:text-white no-underline p-3 md:px-2">About us</a>
+               <a href="/blog" class="text-blue-lightest hover:text-white no-underline p-3 md:px-2">Posts</a>
 
                 <a href="/laravel-people" class="text-blue-lightest hover:text-white no-underline p-3 md:px-2">LaraCelebs</a>
 
