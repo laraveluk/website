ebc8e7b65709b215e1d635fae9ec08809eedbd16
Michael Burton <me@mikeylicio.us>
2018-05-19 16:31:18 +0100

    Move Hero to Partial

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -9,15 +9,14 @@
 @stop
 
 @section('content')
+    <!-- Header -->
     @include('frontend.partials.header')
+    <!-- / Header -->
 
-    <section class="hero bg-cover bg-center bg-no-repeat bg-fixed flex items-center justify-center">
-        <div class="hero-inner text-white text-center">
-            <h1 class="mb-2 text-2xl lg:text-5xl">Welcome to Laravel UK</h1>
-            <h2>The most exciting developer group in the UK</h2>
-        </div>
-    </section>
-    
+    <!-- Hero -->
+    @include('frontend.home.partials.hero')
+    <!-- / Hero -->
+
     <!-- Introduction to LaravelUK -->
     @include('frontend.home.partials.introduction')
     <!-- / Introduction to LaravelUK -->
@@ -25,7 +24,6 @@
     <!-- LaravelUK Sponsors -->
     @include('frontend.home.partials.sponsor')
     <!-- / LaravelUK Sponsors -->
-
 
     <div class="container mx-auto my-6 lg:flex">
         <!-- Latest Posts -->
@@ -40,7 +38,4 @@
         @include('frontend.home.partials.latest-resources')
         <!-- / Latest Resources -->    
     </div>
-
-
-
 @endsection
