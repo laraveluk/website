f5ae404d2e039b8bcb2880eea614310dfedb1001
Michael Burton <me@mikeylicio.us>
2018-05-19 16:17:12 +0100

    Fix the layout of the 'Recent Posts' Block as it was hacked together
    Reformat HTML

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -10,28 +10,30 @@
 
 @section('content')
     @include('frontend.partials.header')
-   <section class="hero bg-cover bg-center bg-no-repeat bg-fixed flex items-center justify-center">
-       <div class="hero-inner text-white text-center">
-           <h1 class="mb-2 text-2xl lg:text-5xl">Welcome to Laravel UK</h1>
-           <h2>The most exciting developer group in the UK</h2>
-       </div>
-   </section>
+    <section class="hero bg-cover bg-center bg-no-repeat bg-fixed flex items-center justify-center">
+        <div class="hero-inner text-white text-center">
+            <h1 class="mb-2 text-2xl lg:text-5xl">Welcome to Laravel UK</h1>
+            <h2>The most exciting developer group in the UK</h2>
+        </div>
+    </section>
 
     <div class="container mx-auto my-6 bg-white p-6">
         <h2 class="mb-4 text-purple-darker">
             Hello and welcome to Laravel UK community.
         </h2>
+
         <p class="leading-normal text-lg mb-4">
             If you are already a member of the LaravelUK Slack community, then you will know what an amazing place it is to be. If you haven’t joined us yet, then you really should.
         </p>
+
         <p class="leading-normal text-lg mb-4">
             The skill levels range from learner to decades of experience in PHP and many other languages and from junior Devs to business owners, entrepreneurs, conference speakers and book authors.
 
-            All you need to do is sign up at
-            <a href="https://laraveluk.signup.team/" class="no-underline text-grey-darkest hover:text-blue-light">https://laraveluk.signup.team/</a>
+            All you need to do is sign up at <a href="https://laraveluk.signup.team/" class="no-underline text-grey-darkest hover:text-blue-light">https://laraveluk.signup.team/</a>
         </p>
+
         <p class="leading-normal text-lg">
-            See you soon :-)
+            See you soon 😁
         </p>
     </div>
 
@@ -45,50 +47,60 @@
             </div>
             <div class="3/4">
                 <p class="mt-6">
-                    Many thanks to <a href="https://snapshooter.io" class="no-underline text-grey-dark">SnapShooter.io</a> for sponsoring the hosting of our website. SnapShooter helps you
-                    to schedule frequent customizable DigitalOcean backups of your droplets and volumes.
+                    Many thanks to <a href="https://snapshooter.io" target="_blank" rel="noopener" class="no-underline text-grey-dark">SnapShooter.io</a> for sponsoring the hosting of our website. SnapShooter helps you to schedule frequent customizable DigitalOcean backups of your droplets and volumes.
                 </p>
             </div>
         </div>
     </div>
 
     <div class="container mx-auto my-6 lg:flex">
-       <div class="sm:w-full mb-2 lg:w-1/3 bg-white lg:mr-4">
-           <h3 class="text-center text-purple-darker my-2">Recent Posts</h3>
-           <div class="content p-4 leading-normal">
-               @foreach ($posts as $post)
-                    <a href="{{ route('frontend.posts.show', [$post->post_type, $post]) }}"
-                       class="no-underline text-grey-darkest hover:text-purple-darkest mb-2">
-                        {{ str_limit($post->title, 40) }}
-                    </a>
-               @endforeach
-
-               <div class="more-posts text-center mt-2">
-                   Read more posts from our
-                   <a href="{{ url('/blog') }}"
-                      class="no-underline text-grey-darkest hover:text-purple-darkest mb-2">
-                       Blog
-                   </a>
-               </div>
-           </div>
-       </div>
         <div class="sm:w-full mb-2 lg:w-1/3 bg-white lg:mr-4">
-            <h3 class="text-center text-purple-darker my-2">Interviews</h3>
+            <h3 class="text-center text-purple-darker my-2">Recent Posts</h3>
             <div class="content p-4 leading-normal">
-                <!-- Hard coded for now -->
-                Laravel UK recently had a chat with one of us, Simon Bennet, creator of Snapshooter.io
-                <a href="/blog/interviews/laravel-talks-to-simon-bennett-of-snapshooter-io-2">
-                    Read more
-                </a>
+                @foreach ($posts as $post)
+                <div class="flex items-center mb-4">
+                    <a href="{{route('profile', $post->author)}}">
+                        <img class="w-10 h-10 rounded-full mr-4" src="{{$post->author->avatar}}" alt="{{$post->author->name}}'s Avatar">
+                    </a>
+                    <div class="text-sm">
+                        <p class="text-black leading-none">
+                            <a href="{{ route('frontend.posts.show', [$post->post_type, $post]) }}" class="no-underline text-grey-darkest hover:text-purple-darkest ">
+                                {{ str_limit($post->title, 40) }}
+                            </a>
+                        </p>
+                        <p class="text-grey-dark">{{$post->created_at->format('jS F Y')}}</p>
+                            @foreach ($post->tags as $tag)
+                            <a href="https://laraveluk.dev/blog/tag/laraveluk" class="no-underline hover:text-purple-darkest">
+                                @if (!$loop->last)
+                                    {{$tag}},
+                                @else
+                                    {{$tag}}
+                                @endif
+                            </a>
+                            @endforeach
+                        </div>
+                    </div>
+                    @endforeach
+
+                    <div class="more-posts text-center mt-2">
+                        Read more posts from our <a href="{{ url('/blog') }}"class="no-underline text-grey-darkest hover:text-purple-darkest mb-2">Blog</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="sm:w-full mb-2 lg:w-1/3 bg-white lg:mr-4">
+                <h3 class="text-center text-purple-darker my-2">Interviews</h3>
+                <div class="content p-4 leading-normal">
+                    <!-- Hard coded for now -->
+                    Laravel UK recently had a chat with one of us, Simon Bennet, creator of Snapshooter.io <a href="/blog/interviews/laravel-talks-to-simon-bennett-of-snapshooter-io-2">Read more</a>
+                </div>
+            </div>
+
+            <div class="sm:w-full mb-2 lg:w-1/3 sm:mb-2 bg-white">
+                <h3 class="text-center text-purple-darker my-2">Resources</h3>
+                <div class="content p-4 leading-normal">
+                    Watch this space for a list of helpful resources and links shared by our members
+                </div>
             </div>
         </div>
-        <div class="sm:w-full mb-2 lg:w-1/3 sm:mb-2 bg-white">
-            <h3 class="text-center text-purple-darker my-2">Resources</h3>
-            <div class="content p-4 leading-normal">
-               Watch this space for a list of helpful resources and links shared by our members
-            </div>
-        </div>
-    </div>
-
-
-@stop
+@endsection
