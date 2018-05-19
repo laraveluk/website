aa69b53cc67f41165c40eef53d714acc9f2e8838
Michael Burton <me@mikeylicio.us>
2018-05-19 16:24:19 +0100

    Move Interviews to Partial

---
+++ resources/views/frontend/home/partials/latest-interviews.blade.php
@@ -0,0 +1,7 @@
+<div class="sm:w-full mb-2 lg:w-1/3 bg-white lg:mr-4">
+    <h3 class="text-center text-purple-darker my-2">Interviews</h3>
+    <div class="content p-4 leading-normal">
+        <!-- Hard coded for now -->
+        Laravel UK recently had a chat with one of us, Simon Bennet, creator of Snapshooter.io <a href="/blog/interviews/laravel-talks-to-simon-bennett-of-snapshooter-io-2">Read more</a>
+    </div>
+</div>
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -28,18 +28,14 @@
 
     <div class="container mx-auto my-6 lg:flex">
         <!-- Latest Posts -->
-        @include('frontend.partials.home.latest-posts')
+        @include('frontend.home.partials.latest-posts')
         <!-- / Latest Posts -->
 
+        <!-- Latest Interviews -->
+        @include('frontend.home.partials.latest-interviews')
+        <!-- / Latest Interviews -->
 
 
-            <div class="sm:w-full mb-2 lg:w-1/3 bg-white lg:mr-4">
-                <h3 class="text-center text-purple-darker my-2">Interviews</h3>
-                <div class="content p-4 leading-normal">
-                    <!-- Hard coded for now -->
-                    Laravel UK recently had a chat with one of us, Simon Bennet, creator of Snapshooter.io <a href="/blog/interviews/laravel-talks-to-simon-bennett-of-snapshooter-io-2">Read more</a>
-                </div>
-            </div>
 
             <div class="sm:w-full mb-2 lg:w-1/3 sm:mb-2 bg-white">
                 <h3 class="text-center text-purple-darker my-2">Resources</h3>
