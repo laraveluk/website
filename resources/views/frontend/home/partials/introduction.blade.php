5ed84481a2f0c9ae7ee3c64a5639b2e8608f534a
Michael Burton <me@mikeylicio.us>
2018-05-19 16:17:47 +0100

    Move introduction to partial.

---
+++ resources/views/frontend/home/partials/introduction.blade.php
@@ -0,0 +1,19 @@
+<div class="container mx-auto my-6 bg-white p-6">
+    <h2 class="mb-4 text-purple-darker">
+        Hello and welcome to Laravel UK community.
+    </h2>
+
+    <p class="leading-normal text-lg mb-4">
+        If you are already a member of the LaravelUK Slack community, then you will know what an amazing place it is to be. If you haven’t joined us yet, then you really should.
+    </p>
+
+    <p class="leading-normal text-lg mb-4">
+        The skill levels range from learner to decades of experience in PHP and many other languages and from junior Devs to business owners, entrepreneurs, conference speakers and book authors.
+
+        All you need to do is sign up at <a href="https://laraveluk.signup.team/" class="no-underline text-grey-darkest hover:text-blue-light">https://laraveluk.signup.team/</a>
+    </p>
+
+    <p class="leading-normal text-lg">
+        See you soon 😁
+    </p>
+</div>
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -16,26 +16,10 @@
             <h2>The most exciting developer group in the UK</h2>
         </div>
     </section>
-
-    <div class="container mx-auto my-6 bg-white p-6">
-        <h2 class="mb-4 text-purple-darker">
-            Hello and welcome to Laravel UK community.
-        </h2>
-
-        <p class="leading-normal text-lg mb-4">
-            If you are already a member of the LaravelUK Slack community, then you will know what an amazing place it is to be. If you haven’t joined us yet, then you really should.
-        </p>
-
-        <p class="leading-normal text-lg mb-4">
-            The skill levels range from learner to decades of experience in PHP and many other languages and from junior Devs to business owners, entrepreneurs, conference speakers and book authors.
-
-            All you need to do is sign up at <a href="https://laraveluk.signup.team/" class="no-underline text-grey-darkest hover:text-blue-light">https://laraveluk.signup.team/</a>
-        </p>
-
-        <p class="leading-normal text-lg">
-            See you soon 😁
-        </p>
-    </div>
+    
+    <!-- Introduction to LaravelUK -->
+    @include('frontend.home.partials.introduction')
+    <!-- / Introduction to LaravelUK -->
 
     <div class="container mx-auto my-6 bg-white p-6">
         <h2 class="mb-4 text-purple-darker">
