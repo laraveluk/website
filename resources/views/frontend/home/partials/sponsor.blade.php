b9eee5ec09e8565623923907421a743ae830c2f2
Michael Burton <me@mikeylicio.us>
2018-05-19 16:18:31 +0100

    Move Sponsor to Partial

---
+++ resources/views/frontend/home/partials/sponsor.blade.php
@@ -0,0 +1,15 @@
+<div class="container mx-auto my-6 bg-white p-6">
+    <h2 class="mb-4 text-purple-darker">
+        Our Sponsors
+    </h2>
+    <div class="flex">
+        <div class="w-1/4">
+            <img class="w-32 h-32" src="https://snapshooter.io/high-restwitter.png" alt="snapshooter.io">
+        </div>
+        <div class="3/4">
+            <p class="mt-6">
+                Many thanks to <a href="https://snapshooter.io" target="_blank" rel="noopener" class="no-underline text-grey-dark">SnapShooter.io</a> for sponsoring the hosting of our website. SnapShooter helps you to schedule frequent customizable DigitalOcean backups of your droplets and volumes.
+            </p>
+        </div>
+    </div>
+</div>
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -20,22 +20,11 @@
     <!-- Introduction to LaravelUK -->
     @include('frontend.home.partials.introduction')
     <!-- / Introduction to LaravelUK -->
+    
+    <!-- LaravelUK Sponsors -->
+    @include('frontend.home.partials.sponsor')
+    <!-- / LaravelUK Sponsors -->
 
-    <div class="container mx-auto my-6 bg-white p-6">
-        <h2 class="mb-4 text-purple-darker">
-            Our Sponsors
-        </h2>
-        <div class="flex">
-            <div class="w-1/4">
-                <img class="w-32 h-32" src="https://snapshooter.io/high-restwitter.png" alt="snapshooter.io">
-            </div>
-            <div class="3/4">
-                <p class="mt-6">
-                    Many thanks to <a href="https://snapshooter.io" target="_blank" rel="noopener" class="no-underline text-grey-dark">SnapShooter.io</a> for sponsoring the hosting of our website. SnapShooter helps you to schedule frequent customizable DigitalOcean backups of your droplets and volumes.
-                </p>
-            </div>
-        </div>
-    </div>
 
     <div class="container mx-auto my-6 lg:flex">
         <div class="sm:w-full mb-2 lg:w-1/3 bg-white lg:mr-4">
