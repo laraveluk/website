c1ebeae38c29417c543d121312723cb93f895b9f
Joshua Franks <joshua@elfradev.com>
2018-05-21 18:00:03 +0100

    Added additional spacing between navigation elements.

--- resources/assets/js/components/Navbar.vue
+++ resources/assets/js/components/Navbar.vue
@@ -13,23 +13,22 @@
             <input type="checkbox" id="mainNavTogger" class="hidden">
             <nav class="invisible fixed pin flex flex-col md:visible md:static md:flex md:flex-row justify-center items-center text-center text-xl md:text-sm lg:text-base bg-blue md:bg-transparent z-10">
                 <label for="mainNavTogger" class="w-10 h-10 flex items-center justify-center p-3 mt-8 mr-6 absolute pin-t pin-r text-2xl leading-none text-white bg-red rounded-full md:hidden">&times;</label>
-               <a href="/" class="text-blue-lightest hover:text-white no-underline p-3 md:px-2">Home</a>
-               <a href="/about" class="text-blue-lightest hover:text-white no-underline p-3 md:px-2">About us</a>
-               <a href="/blog" class="text-blue-lightest hover:text-white no-underline p-3 md:px-2">Posts</a>
+                <a href="/" class="text-blue-lightest hover:text-white no-underline mr-6">Home</a>
+                <a href="/about" class="text-blue-lightest hover:text-white no-underline mr-6">About us</a>
+                <a href="/blog" class="text-blue-lightest hover:text-white no-underline mr-6">Posts</a>
+                <a href="/laravel-people" class="text-blue-lightest hover:text-white no-underline mr-6">LaraCelebs</a>
 
-                <a href="/laravel-people" class="text-blue-lightest hover:text-white no-underline p-3 md:px-2">LaraCelebs</a>
-
-            <div class="guest" v-if="user === null">
-                <a href="/login" class="text-blue-lightest hover:text-white no-underline p-3 md:px-2">Log in</a>
-                <a href="/register" class="text-blue-lightest hover:text-white no-underline p-3 md:px-2">Sign up</a>
-                <!--<a href="/join-slack" class="text-grey bg-red hover:bg-red-dark no-underline rounded-full border border-red-lighter px-4 py-2 lg:ml-2 shadow-lg">Join us on Slack</a>-->
-            </div>
+                <div class="guest" v-if="user === null">
+                    <a href="/login" class="text-blue-lightest hover:text-white no-underline mr-6">Log in</a>
+                    <a href="/register" class="text-blue-lightest hover:text-white no-underline">Sign up</a>
+                    <!--<a href="/join-slack" class="text-grey bg-red hover:bg-red-dark no-underline rounded-full border border-red-lighter px-4 py-2 lg:ml-2 shadow-lg">Join us on Slack</a>-->
+                </div>
 
                 <div v-if="user !== null">
                     <dropdown>
                         <span slot="link" class="appearance-none flex items-center inline-block text-white font-medium bg-blue hover:bg-blue-dark px-4 py-2 rounded">
                             <!--<img class="inline-block h-8 w-8 rounded-full" :src="user.avatar" alt="user.name">-->
-                            <span class="ml-2 mr-1">{{ user.name }}</span>
+                            <span class="mr-1">{{ user.name }}</span>
                             <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                 <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
                             </svg>
