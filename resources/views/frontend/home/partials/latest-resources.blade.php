f677a672fda6becf61519d0c910b83682651e3e4
Joshua Franks <joshua@elfradev.com>
2018-05-21 20:59:11 +0100

    Redesign of latest resources partial.

--- resources/views/frontend/home/partials/latest-resources.blade.php
+++ resources/views/frontend/home/partials/latest-resources.blade.php
@@ -1,6 +1,12 @@
-<div class="sm:w-full mb-2 lg:w-1/3 sm:mb-2 bg-white">
-    <h3 class="text-center text-purple-darker my-2">Resources</h3>
-    <div class="content p-4 leading-normal">
-        Watch this space for a list of helpful resources and links shared by our members
+<div class="flex flex-col w-full rounded shadow border-t-4 border-blue-navy mb-2 p-4 bg-white md:w-1/3 md:mr-4">
+	<h3 class="text-center text-blue-navy border-b-2 border-grey pb-4">Resources</h3>
+    <div class="flex flex-col flex-grow justify-center">
+	    <div class="p-4 leading-normal">
+	        <!-- Hard coded for now -->
+	        <p class="text-grey-darkest text-sm">Watch this space for a list of helpful resources and links shared by our members.</p>
+	    </div>
+    </div>
+    <div class="text-xs text-center text-grey-darker pt-4 border-t border-grey">
+        Coming soon.
     </div>
 </div>
