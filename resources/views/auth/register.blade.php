566931f00615e75fa1a942830db56ddee105b334
Vivian Guillen <me@vivian.do>
2018-06-04 08:33:43 -0400

    Fixes register page on desktop.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,8 +6,8 @@
 
     <div class="w-full">
         <div class="w-full flex flex-col items-center justify-center bg-grey-lighter font-sans min-h-screen-header">
-            <div class="container mx-auto">
-                <div class="md:w-full sm:w-full lg:w-1/2 lg:max-w-l mx-auto border border-grey p-8 md:pt-0 rounded bg-white">
+            <div class="container mx-auto my-8">
+                <div class="md:w-full sm:w-full lg:w-1/2 lg:max-w-l mx-auto border border-grey p-8 my-8 md:pt-0 rounded bg-white">
                     <div class="text-center">
                         <h2 class="text-blue-light text-5xl mb-8 text-center md:-mt-8 bg-grey-lighter inline-block px-4 bg-white border border-grey rounded  py-2">Sign up</h2>
                     </div>
