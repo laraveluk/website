854968d4db7ff645b55bc176bcb7f7cf2627c1e1
DuShaun Alderson-Claeys <dushaun.ac@gmail.com>
2019-04-23 22:51:41 +0100

    commited changes for events page

--- resources/views/pages/event.blade.php
+++ resources/views/pages/event.blade.php
@@ -64,7 +64,11 @@
         <div class="about__container">
             <h1 class="about__title"><span>Friday</span> 26th July 2019</h1>
             <div class="about__content">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam ullam cupiditate earum impedit quibusdam. Iusto dicta cumque amet cum, voluptatum, quas nemo vel rem qui nam perferendis cupiditate atque quibusdam.</p>
+                <p>The LaravelUK group is proud to announce its first annual community event in Old Street, London. After running for almost 3 year, we are finally bringing a nationwide community together.</p>
+                <br>
+                <p>Socialising is the aim of the game here. This event is put together to put faces to names of the people that we all speak to on a daily basis online. But amongst all this, we will have a few talks from people within and outside of the community. All this followed with an evening of drinks.</p>
+                <br>
+                <p>We hope to see as many people as possible there, so don’t hesitate to register your interest above to keep up to date with event info, ticket pricing and some great hotel deals we have lined up for you.</p>
             </div>
         </div>
     </section>
@@ -75,14 +79,15 @@
             <h2 class="speakers__title">Speakers</h2>
         </div>
         <div class="speakers__flex-container">
-            @for ($i = 0; $i < 3; $i++)
-                <article class="speaker">
-                    <div class="speaker__image" style="background-image: url(https://pbs.twimg.com/profile_images/1012406703265124352/eggpA4H6_400x400.jpg)"></div>
-                    <strong class="speaker__name">Mikey Burton</strong>
-                    <p class="speaker__about">Lead Developer / <a href="">Fifteen</a></p>
-                    <p class="speaker__meta"><a href="https://twitter.com/madmikeyb"><i class="fab fa-twitter"></i> @madmikeyb</a></p>
-                </article>
-            @endfor
+            <p>Speakers to be announced soon</p>
+            {{--@for ($i = 0; $i < 3; $i++)--}}
+            {{--<article class="speaker">--}}
+            {{--<div class="speaker__image" style="background-image: url(https://pbs.twimg.com/profile_images/1012406703265124352/eggpA4H6_400x400.jpg)"></div>--}}
+            {{--<strong class="speaker__name">Mikey Burton</strong>--}}
+            {{--<p class="speaker__about">Lead Developer / <a href="">Fifteen</a></p>--}}
+            {{--<p class="speaker__meta"><a href="https://twitter.com/madmikeyb"><i class="fab fa-twitter"></i> @madmikeyb</a></p>--}}
+            {{--</article>--}}
+            {{--@endfor--}}
         </div>
     </section>
     {{-- / Speakers --}}
@@ -90,16 +95,17 @@
     <section class="sponsors">
         <div class="sponsors__container">
             <h2 class="sponsors__title">Sponsors</h2>
-            <p class="sponsors__intro">Thanks to our sponsors</p>
+            {{--<p class="sponsors__intro">Thanks to our sponsors</p>--}}
         </div>
         <div class="sponsors__flex-container">
-            @for ($i = 0; $i < 3; $i++)
-                <article class="sponsor">
-                    <a href="#" class="sponsor__image">
-                        <img src="https://www.google.com/logos/doodles/2015/googles-new-logo-5078286822539264.3-hp2x.gif" alt="Sponsor">
-                    </a>
-                </article>
-            @endfor
+            <p>Coming soon</p>
+            {{--@for ($i = 0; $i < 3; $i++)--}}
+            {{--<article class="sponsor">--}}
+            {{--<a href="#" class="sponsor__image">--}}
+            {{--<img src="https://www.google.com/logos/doodles/2015/googles-new-logo-5078286822539264.3-hp2x.gif" alt="Sponsor">--}}
+            {{--</a>--}}
+            {{--</article>--}}
+            {{--@endfor--}}
         </div>
     </section>
     {{-- / Sponsors --}}
@@ -108,7 +114,7 @@
         <div class="map__container">
             <h2 class="map__title">Venue</h2>
         </div>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2481.7403810218825!2d-0.08046558353968311!3d51.53632147963979!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761c9624428cb5%3A0xee818efd911009c5!2sTuscany+Wharf%2C+4B+Orsman+Rd%2C+London+N1+5QJ!5e0!3m2!1sen!2suk!4v1555347204380!5m2!1sen!2suk" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9926.961519646988!2d-0.0782769!3d51.5363215!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7f6f791530d5b70b!2sThe+Flowerpot+Hoxton!5e0!3m2!1sen!2suk!4v1556055749809!5m2!1sen!2suk" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
     </section>
     {{-- / Map --}}
     <footer class="footer">
