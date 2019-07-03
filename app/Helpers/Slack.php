da1ddd605e0048e6ed95de146d1c364e61e124c6
Michael Burton <me@mikeylicio.us>
2019-07-03 16:14:16 +0100

    Disallow Slack Invitations
    Temporarily disallow slack invitations to stop the influx of spambots.

--- app/Helpers/Slack.php
+++ app/Helpers/Slack.php
@@ -13,6 +13,9 @@ class Slack
 
     public static function sendInvitation($email) {
         //return \Unirest\Request::post(config('slack.invitation_url'), [], ['token' => config('slack.legacy_token'), 'email' => $email]);
+        \Log::info($email . ' attempted to obtain an invitation to Slack');
+        return false;
+        /**
         $client = new \GuzzleHttp\Client();
         $response =  $client->request('POST', config('slack.invitation_url'), ['query' => [
             'token' => config('slack.legacy_token'),
@@ -20,6 +23,7 @@ class Slack
         ]]);
         $response = json_decode($response->getBody());
         return $response;
+        */
     }
 
 }
